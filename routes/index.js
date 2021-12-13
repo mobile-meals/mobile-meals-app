56e0b7cb5bfb9d7e38e449254a00eb6051b108bb
Uditha Melan <melan.udi@gmail.com>
2021-12-14 00:56:56 +0530

    initial-setup

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 // Welcome Page
-router.get('/', (req, res) => res.render('auth/login'));
+router.get('/', (req, res) => res.render('auth/Otp'));
 
 module.exports = router;
