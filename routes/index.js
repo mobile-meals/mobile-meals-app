1f1189e627df5b71d34895a0608937e110d1301a
Uditha Melan <melan.udi@gmail.com>
2021-12-29 23:53:21 +0530

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 // Welcome Page
-router.get('/', (req, res) => res.render('components'));
+router.get('/', function(req, res) {
+
+    topRestaurants = [
+        {
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/07/29/05/52/burger-4369973_960_720.jpg',
+            isBanner : false,
+            bannerText: '',
+            restaurantTitle: 'The American Touch',
+            restaurantRating: '4.5',
+        }
+    ];
+
+    res.render('home', {
+        
+    });
+});
+
+router.get('/components', (req, res) => res.render('components'));
 
 module.exports = router;
